251adce212bb136d06eacdebbcd43a38ad58d76d
igorliotta <liottaigor@gmail.com>
2023-10-06 16:09:16 +0200

    Fix:

--- js/app.js
+++ js/app.js
@@ -1,28 +1,24 @@
-console.log('Ciao a tutti ragazzi');
+console.log("Ciao a tutti ragazzi");
 
 for (let i = 0; i < 100; i++) {
-    let n = i + 1;
-    console.log(n);
+  let n = i + 1;
 
-   const resto = n % 3;
-   let multipleOf3 = 'Fizz';
+  const resto = n % 3;
+  let multipleOf3 = "Fizz";
 
-   const resto2 = n % 5;
-   let multipleOf5 = 'Buzz';
+  const resto2 = n % 5; 
+  let multipleOf5 = "Buzz";
 
-//    const restoMultipli = (n % 3) & (n % 5);
-   let multipleOfBoth = 'FizzBuzz';
+ 
+  let multipleOfBoth = "FizzBuzz";
 
-   if (resto === 0) {
+  if ((resto === 0) && (resto2 === 0)) { 
+    console.log(multipleOfBoth);
+  } else if (resto2 === 0) { 
+    console.log(multipleOf5); 
+  } else if (resto === 0) { 
     console.log(multipleOf3);
-   }
-      else if (resto2 === 0) {
-        console.log(multipleOf5);
-    }
-       else if (resto && resto2) {
-        console.log(multipleOfBoth);
-      }
-
-
-
+  } else {
+    console.log(n);
+  }
 }
