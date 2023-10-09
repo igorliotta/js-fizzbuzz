818f399ec1dd6ac5bcab597d4656bd56d6e83ece
igorliotta <liottaigor@gmail.com>
2023-10-09 09:26:09 +0200

    Fix:

--- js/app.js
+++ js/app.js
@@ -22,3 +22,35 @@ for (let i = 0; i < 100; i++) {
     console.log(n);
   }
 }
+
+
+// const gridDOMElement = document.querySelector('.grid');
+// console.log(gridDOMElement);
+
+// for (let i = 0; i < 100; i++) {
+
+//     let textString
+//     const boxString = '<div class="box">' + textString + '</div>';
+//     gridDOMElement.innerHTML = boxString;
+
+//     if ((resto === 0) && (resto2 === 0)) { 
+
+//         textString = multipleOfBoth;
+//         console.log(gridDOMElement);
+
+//       } else if (resto2 === 0) { 
+
+//         textString = multipleOf5;
+//         console.log(gridDOMElement);
+
+//       } else if (resto === 0) { 
+
+//         textString = multipleOf3;
+//         console.log(gridDOMElement);
+        
+
+//       } else {
+
+//         console.log(n);
+//       }
+// }
